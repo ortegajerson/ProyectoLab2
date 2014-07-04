4867219973a1ce8cb3623567947619507015f1da
ortegajerson <j.ortega03@ufromail.cl>
2014-07-04 14:54:46 -0400

    aparece menu y sale el logear

--- Proyecto/src/Logear.java
+++ Proyecto/src/Logear.java
@@ -42,6 +42,11 @@ public class Logear extends javax.swing.JFrame {
         jLabel2.setText("Nombre");
 
         jButton1.setText("Comenzar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -78,6 +83,12 @@ public class Logear extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        m.inicio.setVisible(true);
+        m.logear.setVisible(false);
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
